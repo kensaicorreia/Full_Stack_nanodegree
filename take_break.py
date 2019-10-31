62796c8deccaef9a841fd630f90423d4697f26d6
Kensai Correia <kensaicorreia1@gmail.com>
2019-10-31 12:42:38 -0400

    take a break program

---
+++ take_break.py
@@ -0,0 +1,15 @@
+import os
+import time
+
+'''
+this will pop up three time to play a song
+'''
+total_breaks = 3
+break_count = 0
+while(break_count < total_breaks):
+    wait = time.sleep(10)
+    browser_for_file = os.startfile('C:\\Users\\linux\\Music\\take_a_break.mp3')
+    break_count = break_count + 1
+
+
+
