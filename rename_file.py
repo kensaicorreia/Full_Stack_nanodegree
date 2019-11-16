1e03d8309a0bfe2061cb18a3e321bc85769ef402
Kensai Correia <https://github.com/kensaicorreia/Deep-Learning>
2019-11-16 13:05:12 -0400

    add comment

--- rename_file.py
+++ rename_file.py
@@ -1,16 +1,19 @@
+'''this script is use to rename file that have number in front of the file name 
+and are seprated with space and have and index number at the back fo the file name '''
+
 import os
 
 # change directory 
 os.chdir("C:/Users/linux/Documents/New folder")
 #print(os.getcwd())
 for f_name in os.listdir():
-    #print(f_name)
+    #print(f_name) # test
     file_name, file_ext = os.path.splitext(f_name)
-    #print(file_name)
+    #print(file_name) # test 
     # split file name and separate  extension 
     title_front, file_title, title_back = file_name.split(' ')
     # split the file name if separate by space
-    #print(title_back)
+    #print(title_back) # test
     
     title_front = title_front.strip('0123456789')
     # strip any number from the front of the name 
