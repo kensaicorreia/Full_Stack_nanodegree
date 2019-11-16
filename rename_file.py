647b09a2237ee2250754afcbf68d5181528570b4
Kensai Correia <https://github.com/kensaicorreia/Deep-Learning>
2019-11-16 12:48:31 -0400

    rename

---
+++ rename_file.py
@@ -0,0 +1,25 @@
+import os
+
+# change directory 
+os.chdir("C:/Users/linux/Documents/New folder")
+#print(os.getcwd())
+for f_name in os.listdir():
+    #print(f_name)
+    file_name, file_ext = os.path.splitext(f_name)
+    #print(file_name)
+    # split file name and separate  extension 
+    title_front, file_title, title_back = file_name.split(' ')
+    # split the file name if separate by space
+    #print(title_back)
+    
+    title_front = title_front.strip('0123456789')
+    # strip any number from the front of the name 
+    title_back = title_back.strip()[:].zfill(3)
+    # strip any space from the back and zero fill any number to 2 place holder
+
+    new_name = '{}-{}{}'.format(file_title,title_back, file_ext)
+    os.rename(f_name, new_name)
+    print(new_name)
+
+        
+
--- take_break.py
+++ take_break.py
@@ -4,12 +4,14 @@ import time
 '''
 this will pop up three time to play a song
 '''
+
 total_breaks = 3
 break_count = 0
 while(break_count < total_breaks):
     wait = time.sleep(10)
     browser_for_file = os.startfile('C:\\Users\\linux\\Music\\take_a_break.mp3')
     break_count = break_count + 1
+
 
 
 
